4e5a6603a5b0d98fbeee8111c31fff10154b6f31
Lemonchis <127582078+Lemonchis@users.noreply.github.com>
2023-03-11 07:36:39 +0200

    Add files via upload

---
+++ repetition.js
@@ -0,0 +1,20 @@
+// Визначення типу значення, оператор typeof
+
+// const productName = 'Mango';
+// const pricePerItem = 150;
+// const isOnSale = false;
+// const error = null;
+// let quantity;
+
+// .....................
+// Шаблонні строки
+// // Написати фразу за допомогою шаблонних строк Привіт, меня  звати < імя >, мні  < стільки-то > років і мні подобається < хоббі >, де дані з < ..... > - змінні вставлені в строку.
+
+// const name = '';
+// const age = ;
+// const hobby = '';
+// const message = ;
+
+// console.log(message);
+
+// .......................
